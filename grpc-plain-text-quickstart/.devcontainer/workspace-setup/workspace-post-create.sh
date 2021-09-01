6acdf962ac72b33b59d8adcfc68a0eccdc74ed48
Marc Hildenbrand <mhilde@canva.com>
2021-09-01 17:13:39 +1000

    Centralize .devcontainer with symlinks

--- grpc-plain-text-quickstart/.devcontainer/workspace-setup/workspace-post-create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-WORKSPACE_FOLDER=${1}
-
-rsync -a .devcontainer/workspace-setup/ ${WORKSPACE_FOLDER}/.vscode/ --ignore-existing
-
-#sudo pip install -r requirements.txt
